5d160332958d425973f0ff0f35e435b514063533
Fatih Turgut <ftfatihft@gmail.com>
2018-08-25 16:12:14 +0300

    button disabled feature added

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,13 @@ import classes from './BuildControl.css'
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <div className={classes.Less} onClick={props.removed}>Less</div>
-    <div className={classes.More} onClick={props.added}>More</div>
+    <div 
+      className={classes.Less} 
+      onClick={props.removed}
+      disabled={props.disabled}>Less</div>
+    <div 
+      className={classes.More} 
+      onClick={props.added}>More</div>
   </div>
 );
 
