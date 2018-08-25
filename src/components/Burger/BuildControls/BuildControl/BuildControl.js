f20fdd432865876d08445095cd468394cbac759f
Fatih Turgut <ftfatihft@gmail.com>
2018-08-25 15:04:41 +0300

    removeIngredientHandler done

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ import classes from './BuildControl.css'
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <div className={classes.Less}>Less</div>
+    <div className={classes.Less} onClick={props.removed}>Less</div>
     <div className={classes.More} onClick={props.added}>More</div>
   </div>
 );
