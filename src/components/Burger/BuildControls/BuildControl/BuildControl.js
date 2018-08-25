d016aaba79851e1f9f159bb471bc7e586089721b
Fatih Turgut <ftfatihft@gmail.com>
2018-08-25 23:16:57 +0300

    css bugs fixed

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,13 +5,13 @@ import classes from './BuildControl.css'
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <div 
+    <button 
       className={classes.Less} 
       onClick={props.removed}
-      disabled={props.disabled}>Less</div>
-    <div 
+      disabled={props.disabled}>Less</button>
+    <button 
       className={classes.More} 
-      onClick={props.added}>More</div>
+      onClick={props.added}>More</button>
   </div>
 );
 
