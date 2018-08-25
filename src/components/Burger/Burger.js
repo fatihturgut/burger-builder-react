92b74986ea98bdac3fafed11b765471bcb374754
Fatih Turgut <ftfatihft@gmail.com>
2018-08-25 14:13:00 +0300

    Removed items inside burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,16 +4,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-      return [...Array(props.ingredients[igKey])].map((A, index) => {
-        // array içerisi undefined geliyor, bu yani  kullanmadığımız "A" değişkeni
-        // console.log([...Array(props.ingredients[igKey])])
-        // console.log("igKey", igKey);
-        // console.log("index", index);
-        return <BurgerIngredient key={igKey + index} type={igKey} />;
-      });
+  let transformedIngredients = Object.keys(props.ingredients)
+  .map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((A, index) => {
+      // array içerisi undefined geliyor, bu yani  kullanmadığımız "A" değişkeni
+      // console.log([...Array(props.ingredients[igKey])])
+      // console.log("igKey", igKey);
+      // console.log("index", index);
+      return <BurgerIngredient key={igKey + index} type={igKey} />;
     });
+  })
+  .reduce((arr, el) => { 
+    return arr.concat(el);
+  }, []);
+
+  console.log("transformedIngredients", transformedIngredients)
+
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients.</p>
+  }
 
   return (
     <div className={classes.Burger}>
