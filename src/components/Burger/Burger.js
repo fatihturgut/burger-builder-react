ad77b3166a323f5740a844da5df69a63dbcab15b
Fatih Turgut <ftfatihft@gmail.com>
2018-08-25 13:12:07 +0300

    burger builder dynamically create burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((A, index) => {
+        // array içerisi undefined geliyor, bu yani  kullanmadığımız "A" değişkeni
+        // console.log([...Array(props.ingredients[igKey])])
+        // console.log("igKey", igKey);
+        // console.log("index", index);
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
