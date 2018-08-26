e6601e995969a1055ce1293e64cfb30964cf8df5
Fatih Turgut <ftfatihft@gmail.com>
2018-08-26 11:49:44 +0300

    continue and cancel done on modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Aux from '../../../hoc/Auxiliary'
+import Aux from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -15,13 +16,15 @@ const orderSummary = (props) => {
     })
 
   return (
-    <Aux>
+    <Aux> 
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>
         {ingredientSummary}
       </ul>
       <p>Continue to Checkout</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   );
 };
